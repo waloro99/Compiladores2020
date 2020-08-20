8f19b03b9007bc6ec1fa8c75a113dc04489bd044
waloro <walterof1@hotmail.com>
2020-08-19 22:56:58 -0600

    create interfaz

--- minic/Form1.Designer.cs
+++ minic/Form1.Designer.cs
@@ -28,20 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.textBox1.Enabled = false;
+            this.textBox1.ForeColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(12, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(543, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(274, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MINI C#";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(568, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(259, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "ACEPTAR";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(567, 204);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(680, 164);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- minic/Form1.cs
+++ minic/Form1.cs
@@ -12,9 +12,63 @@ namespace minic
 {
     public partial class Form1 : Form
     {
+        // ----------------------------------------VAR GLOBAL-------------------------------------------
+
+        public string PathFile = string.Empty; // var path the file
+
         public Form1()
         {
             InitializeComponent();
+
+            //put button
+            button1.MouseClick += this.Press_button;
+
+            //mouse move
+            button1.MouseLeave += this.Quit_button;
+
+            //instance
+            Bitmap imagen = new Bitmap(Application.StartupPath + @"\img\ima_archivo1.png");
+            button1.Image = imagen;
+
         }
+
+        #region Button_Search
+
+        //method for back imagen original 
+        private void Quit_button(object obj, EventArgs evt)
+        {
+            Bitmap imagen = new Bitmap(Application.StartupPath + @"\img\ima_archivo1.png");
+            button1.Image = imagen;
+        }
+
+        //method for change imagen dowload
+        private void Press_button(object obj, EventArgs evt)
+        {
+            Bitmap imagen = new Bitmap(Application.StartupPath + @"\img\ima_archivo2.png");
+            button1.Image = imagen;
+        }
+
+        #endregion
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text documents (.txt)|*.txt"; //only file .txt
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                PathFile = openFileDialog.FileName;
+                button2.Enabled = true;
+                textBox1.Enabled = true;
+            }
+
+            textBox1.Text = PathFile;
+        }
+
+
     }
 }
