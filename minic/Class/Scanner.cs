a748cc160d9461e25b0f89d39e5448bec4384583
waloro <walterof1@hotmail.com>
2020-08-21 16:53:20 -0600

    new method

--- minic/Class/Scanner.cs
+++ minic/Class/Scanner.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace minic.Class
@@ -13,14 +14,16 @@ namespace minic.Class
 
         #region FUNCTIONS PUBLIC
 
+        //List for save data the lexic analysis
+        public List<string> NewFile = new List<string>();
+        public int column = 0;
+        public int row = 0;
+
         //method public for scanner the file
         public List<string> Scanner_Lexic(string[] file )
         {
-            //List for save data the lexic analysis
-            List<string> NewFile = new List<string>();
-
-
-
+            //scrolll the array
+            Scanner_Private(file);
             return NewFile; // return the new file for write 
         }
 
@@ -30,7 +33,35 @@ namespace minic.Class
 
         #region FUNCTIONS PRIVATE
 
+        //method for analysis the file
+        private void Scanner_Private(string[] file)
+        {
+            //scroll the file
+            for (int i = 0; i < file.Length; i++) // i = fila
+            {
+                //separate the lines
+                //separate the words
+                string[] word = Regex.Split(file[i]," ");
+                row ++; //the line increment 
+                Filter_First(word,row);
+            }
 
+        }
+
+        //method for first filter, parameters words and line
+        private void Filter_First(string[] word, int line)
+        {
+            //scroll the array character by character
+            for (int i = 0; i < word.Length; i++)
+            {
+                //type
+                if (Regex.IsMatch(word[i], @""))
+                {
+
+                }
+            }
+
+        }
 
         #endregion
 
