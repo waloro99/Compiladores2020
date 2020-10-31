19c83b719f7329684a67c4328e142d42e459bc60
Ricardo Chian <ricardochianc@gmail.com>
2020-10-31 05:45:17 -0600

    Se corrigió error

--- minic/Class/Fase 2/LR1.cs
+++ minic/Class/Fase 2/LR1.cs
@@ -220,7 +220,7 @@ namespace minic.Class.Fase_2
                     break;
 
                 default:
-                    itemtoSearch = itemtoSearch.Replace("T_", "");
+                    itemtoSearch = type.description.Replace("T_", "");
                     break;
             }
 
