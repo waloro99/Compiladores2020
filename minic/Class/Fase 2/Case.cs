ac7442bb9e338ef19bcf775d9c9724042bb01acc
Ricardo Chian <ricardochianc@gmail.com>
2020-10-31 04:15:27 -0600

    Lectura y carga de tabla de análisis

---
+++ minic/Class/Fase 2/Case.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minic.Class.Fase_2
+{
+    public class Case
+    {
+        public string Match { get; set; }
+        public List<string> Actions { get; set; }
+
+        public Case(string match)
+        {
+            Match = match;
+            Actions = new List<string>();
+        }
+    }
+}
--- minic/Class/Fase 2/LR1.cs
+++ minic/Class/Fase 2/LR1.cs
@@ -10,19 +10,32 @@ namespace minic.Class.Fase_2
     public class LR1
     {
         public List<Production> Grammar { get; set; }
-
         public List<Production> CanonicalCollection { get; set; }
+        public List<Case> AnalysisTable{ get; set; }
 
-        private string Path { get; set; }
+        private string PathConfig { get; set; }
+
+        private string PathTable { get; set; }
 
         public LR1()
         {
             Grammar = new List<Production>();
-            Path = Environment.CurrentDirectory + "\\config.txt";
+            AnalysisTable = new List<Case>();
+            PathConfig = Environment.CurrentDirectory + "\\config.txt";
+            PathTable = Environment.CurrentDirectory + "\\Tabla.csv";
 
-            if (File.Exists(Path))
+            if (File.Exists(PathConfig))
             {
-                setGrammar(Path);
+                setGrammar(PathConfig);
+
+                if (File.Exists(PathTable))
+                {
+                    setAnalysisTable(PathTable);
+                }
+                else
+                {
+                    throw new Exception("Analysis Table file not found");
+                }
             }
             else
             {
@@ -124,6 +137,51 @@ namespace minic.Class.Fase_2
 
 
             return found;
+        }
+
+        private void setAnalysisTable(string path)
+        {
+            var lines = new List<string>();
+
+            using (var file = new FileStream(path, FileMode.Open))
+            {
+                using (var reader = new StreamReader(file))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        lines.Add(reader.ReadLine());
+                    }
+                }
+            }
+
+            var firstLineContent = lines[0].Split('|').ToList();
+            
+            foreach (var line in firstLineContent)
+            {
+                if (line != "state")
+                {
+                    AnalysisTable.Add(new Case(line));
+                }
+            }
+
+            firstLineContent.RemoveAt(0);
+
+            for (int i = 0; i < AnalysisTable.Count; i++)
+            {
+                foreach (var item in lines)
+                {
+                    var action = item.Split('|')[i];
+
+                    if (action == "")
+                    {
+                        AnalysisTable[i].Actions.Add("-1");
+                    }
+                    else
+                    {
+                        AnalysisTable[i].Actions.Add(action);
+                    }
+                }
+            }
         }
     }
 }
